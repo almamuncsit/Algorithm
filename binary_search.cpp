7dd85ae1637d078d9ad04b80a257587af1985c71
Md. Al-Mamun Sarkar <almamun.csit@gmail.com>
2015-05-20 16:44:50 +0600

    Update binary_search.cpp

--- binary_search.cpp
+++ binary_search.cpp
@@ -2,11 +2,15 @@
 using namespace std;
 int main()
 {
+    // initial data in a array
     int data[15] = {1, 3, 4, 6, 7, 8, 9, 12, 14, 16, 19, 20, 23, 25, 48};
+    //declear variable;
     int item, low = 0, high = 14, mid, possition = 0;
+    // Get input element for search 
     cin>>item;
     // Search Data
     while (low <= high) {
+         // Genarate Mid point of affected array
         mid = low + (high-low)/2;
         if (item == data[mid]){
             possition = mid+1;
@@ -18,9 +22,10 @@ int main()
         }
     }
 
-    if (possition > 0 )
+    
+    if (possition > 0 ) // Print outpur if found
         cout<< "Item is found at location " <<possition<<endl;
-    else
+    else // Print outpur if not found
         cout << "Not exist!" << endl;
 
     return 0;
