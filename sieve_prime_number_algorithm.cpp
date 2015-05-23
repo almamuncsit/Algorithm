008e4f5489f59508adb5b6e86d34fe00b99fc596
Md. Al-Mamun Sarkar <almamun.csit@gmail.com>
2015-05-24 00:20:56 +0600

    Update sieve_prime_number_algorithm.cpp
    sieve prime number algorithm

--- sieve_prime_number_algorithm.cpp
+++ sieve_prime_number_algorithm.cpp
@@ -9,14 +9,15 @@ int main()
     cout<<"Enter Maximum length of data ";
     cin>>length;
 
-
+    // Make all possible element 1
     array[2] = 1;
     for (int i = 3; i<=length; i+=2) {
         array[i] = 1;
     }
-
+    // Get square root of length of array
     int n = (int)sqrt(length) +1;
 
+    //Filter prime number
     for (int i=3; i<=n; i++) {
         if(array[i] == 1) {
             for (int j=i*i; j<length; j+=i) {
@@ -25,7 +26,7 @@ int main()
         }
     }
 
-
+    // Print Prime numbers
     cout<<"Print Prime Numbers "<<endl;
     for (int i = 2; i<=length; i++) {
         if (array[i] == 1) {
